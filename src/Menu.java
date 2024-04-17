353916c27e60c20b7454a84e3ccb5665a0733d83
unknown <zaml56@naver.com>
2024-04-17 22:22:26 +0900

    menu 수정

--- src/Menu.java
+++ src/Menu.java
@@ -1,7 +1,42 @@
+import java.util.Scanner;
+import JDBC.DBConnect;
 
 public class Menu {
-	public static void main(String[] args) {
-		
-	}
+   
+   static Scanner sc = new Scanner(System.in);
+   
+   public static void main(String[] args) {
 
+	  
+      while(true) {
+         // 고객 메뉴 ui
+         System.out.println("[[ PITCHER GAME ]]");
+         System.out.println("-- 게임 시작 : S, 랭킹 : R, 게임 종료 : E --");
+         System.out.print(">>");
+         String menu = sc.next();
+         menu =  menu.toLowerCase();
+         
+         switch(menu.charAt(0)) {
+         	case 'ㄴ':
+         	case 's':
+         		System.out.println("게임을 시작합니다.");
+         		Event e = new Event(sc);
+         		break;
+         	case 'ㄱ':
+         	case 'r':
+         		System.out.println("랭킹을 불러오겠습니다.");
+         		DBConnect dao = new DBConnect();
+         		dao.allPlay();
+         	case 'ㄷ':
+         	case 'e':
+         		System.out.println("게임이 끝났습니다.");
+         		sc.close();
+         		System.exit(0);
+         	default:
+         		System.out.println("잘못된 값을 입력하셨습니다. 다시 입력해주세요.");
+         		break;
+         }
+         
+      }
+   }
 }
